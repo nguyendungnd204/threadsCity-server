000d242f47a155d978ca829c28fb087ab30ff837
nguyendungnd204 <ngvandungnvd204@gmail.com>
2025-03-31 12:49:50 +0700

    updated create thread

--- controllers/thread.controller.js
+++ controllers/thread.controller.js
@@ -1,19 +1,24 @@
 const ThreadService = require('../services/thread.service');
 
 exports.createThread = async (req, res) => {
-    const {id} = req.params;
-    const {content, image} = req.body;
+    try {
+        const {authorId} = req.query;
+        const {content, image} = req.body;
 
-    if(content == ''){
-        return res.status(400).json({
-            message: 'content is required',
-            success: false
+        if(content == ''){
+            return res.status(400).json({
+                message: 'content is required',
+                success: false
+            })
+        }
+
+        const thread = await ThreadService.createThreadService(authorId, content, image);
+        return res.status(201).json({
+            message: 'thread created successfully',
+            data: thread
         })
+    } catch (error) {
+        console.log(error);
     }
-
-    const thread = await ThreadService.createThreadService(id, content, image);
-    return res.status(201).json({
-        message: 'thread created successfully',
-        data: thread
-    })
+    
 }
--- services/thread.service.js
+++ services/thread.service.js
@@ -1,10 +1,9 @@
 const Thread = require('../models/thread.model');
 
 class ThreadService {
-    async createThreadService(id, content, image){
-        const thread = await Thread.create({content, image});
+    async createThreadService(authorId, content, image){
+        const thread = await Thread.create({authorId, content, image});
         return thread;
     }
 }
-
 module.exports = new ThreadService();
